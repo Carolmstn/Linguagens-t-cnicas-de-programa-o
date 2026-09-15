7c0d4395e0234b6562bd52d1956f1b5801824b26
Carolmstn <carolmestriner@outlook.com.br>
2026-09-15 17:30:29 -0300

    Update and rename Aula 10.09.c to Contra-Cheque.c

--- Contra-Cheque.c
+++ Contra-Cheque.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
 	
 	salario_bruto = horas * valor;
 	
-	desconto = calc_inss(salario);
+	desconto = calc_inss(salario_bruto);
 	
 	imposto = calc_irpf(salario_bruto);
 	
