75bdc2193e07f8c70d6d04f3498d25aa95c8194a
Carolmstn <carolmestriner@outlook.com.br>
2026-09-15 17:02:34 -0300

    Update Aula 10.09.c

--- Aula 10.09.c
+++ Aula 10.09.c
@@ -8,33 +8,42 @@ float calc_inss (float salario){
 	else return salario * 0.14;
 }
 
-float calc_irpf (float salario){
-	if (salario - calc_inss(salario) <= 2259.20) return salario * 0;
-	else if (salario - calc_inss(salario) <= 2826.65) return salario * 0.075 - 169.44;
-	else if (salario - calc_inss(salario) <= 3751.05) return salario * 0.15 - 381.44;
-	else if (salario - calc_inss(salario) <= 4664.68) return salario * 0.225 - 662.77;
-	else return salario - calc_inss(salario) * 0.275 - 896.00;
+float calc_irpf (float salario){ 
+	
+	float base_calculo = salario - calc_inss(salario);
+	
+	if (base_calculo <= 2259.20) return salario * 0;
+	else if (base_calculo <= 2826.65) return salario * 0.075 - 169.44;
+	else if (base_calculo <= 3751.05) return salario * 0.15 - 381.44;
+	else if (base_calculo <= 4664.68) return salario * 0.225 - 662.77;
+	else return base_calculo * 0.275 - 896.00;
 }
 
 int main(int argc, char *argv[]) {
 	
 	int horas;
-	float salario, salario_b, desconto, imposto, valor, salario_bruto, liquido;
-	printf("Digite seu salario: ");
-	scanf("%f", &salario);
+	float valor, salario_bruto, desconto, imposto, liquido;
 	
 	printf("Digite o valor da sua hora e a quantidade trabalhada: ");
 	scanf("%f %d", &valor, &horas);
 	
-	salario_b = horas * valor;
+	salario_bruto = horas * valor;
 	
 	desconto = calc_inss(salario);
 	
 	imposto = calc_irpf(salario_bruto);
 	
-	liquido = salario - desconto - imposto;
+	liquido = salario_bruto - desconto - imposto;
 	
-	printf("======================================================\n   RECIBO DE PAGAMENTO DE SALARIO (CONTRA-CHEQUE)\n======================================================\n  Salario Bruto (Horas x Valor):   R$ %f\n  (-) Desconto INSS:                R$ %f\n  (-) Desconto IRPF:                R$ %f\n ------------------------------------------------------\n   LIQUIDO A RECEBER:               R$ %f\n======================================================", salario_b, desconto, imposto, liquido);
+	printf("======================================================\n");
+	printf(" RECIBO DE PAGAMENTO DE SALARIO (CONTRA-CHEQUE)\n");
+	printf("======================================================\n");
+	printf(" Salario Bruto (Horas x Valor): R$ %.2f\n", salario_bruto);
+	printf(" (-) Desconto INSS:             R$ %.2f\n", desconto);
+	printf(" (-) Desconto IRPF:             R$ %.2f\n", imposto); 
+	printf("------------------------------------------------------\n");
+	printf(" LIQUIDO A RECEBER:             R$ %.2f\n", liquido);
+	printf("======================================================\n");
 
 	return 0;
 }
